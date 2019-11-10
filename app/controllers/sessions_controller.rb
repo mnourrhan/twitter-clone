95be37c3f6e8eff3ae49d08d891dc74dd611bc5f
mnourrhan <mnourrhan@gmail.com>
2019-11-10 16:20:21 +0200

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1'? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1'? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated"
+        message += "Check your email for the activation link"
+        flash[:warning] = message
+        redirected_to root_url
+      end
     else
-      # .now used to render the flash message only in the current page
+      # now used to render the flash message only in the current page
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # /account_activation/<token>/edit
+  resources :account_activations, only: [:edit]
 end
